0b8ef1a1fe518b4932a33ef646f57a002f430c12
SelenaMendia <103127457+SelenaMendia@users.noreply.github.com>
2022-11-14 22:53:14 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace activ._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero=0, suma=0, promedio;
+            int [] arregloNumerico = new int [5];
+
+            for (int i = 0; i < arregloNumerico.Length;i++)
+            {
+                Console.WriteLine("Ingrese el numero que quiere almacenar del elemento de indice {0} :", i);
+                numero=Convert.ToInt32(Console.ReadLine());
+                arregloNumerico[i]=numero;
+                suma=suma+arregloNumerico[i];
+
+            }
+            promedio=suma/arregloNumerico.Length;
+
+            Console.WriteLine("El promedio de los numeros ingresados es: {0}%",promedio);
+            
+        }
+    }
+}
